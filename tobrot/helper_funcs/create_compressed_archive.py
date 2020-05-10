4b8a6652503f2efdf0048b59a115aeccfff743b8
AK25 <60230389+cdfxscrq@users.noreply.github.com>
2020-05-10 13:16:17 +0530

    compressed change to rar

--- tobrot/helper_funcs/create_compressed_archive.py
+++ tobrot/helper_funcs/create_compressed_archive.py
@@ -19,16 +19,17 @@ async def create_archive(input_directory):
     return_name = None
     if os.path.exists(input_directory):
         base_dir_name = os.path.basename(input_directory)
-        compressed_file_name = f"{base_dir_name}.tar.gz"
+        compressed_file_name = f"{base_dir_name}.rar"
         # #BlameTelegram
         suffix_extention_length = 1 + 3 + 1 + 2
         if len(base_dir_name) > (64 - suffix_extention_length):
             compressed_file_name = base_dir_name[0:(64 - suffix_extention_length)]
-            compressed_file_name += ".tar.gz"
+            compressed_file_name += ".rar"
         # fix for https://t.me/c/1434259219/13344
         file_genertor_command = [
-            "tar",
-            "-zcvf",
+            "rar",
+            "a",
+            "-m0",
             compressed_file_name,
             f"{input_directory}"
         ]
@@ -42,6 +43,8 @@ async def create_archive(input_directory):
         stdout, stderr = await process.communicate()
         e_response = stderr.decode().strip()
         t_response = stdout.decode().strip()
+        LOGGER.info("============os.listdir()===============")
+        LOGGER.info(os.listdir())
         if os.path.exists(compressed_file_name):
             try:
                 shutil.rmtree(input_directory)
