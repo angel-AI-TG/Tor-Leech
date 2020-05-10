0eb9871325804629b93f0f7f0a45de4631e5b8b4
AK25 <60230389+cdfxscrq@users.noreply.github.com>
2020-05-10 13:43:27 +0530

    Some updated

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -237,11 +237,11 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 #
                 msg = f"\nDownloading File: `{downloading_dir_name}`📥 "
                 msg += f"\nSpeed: {file.download_speed_string()} ⬇️ / {file.upload_speed_string()} ⬆️"
-                msg += f"\nProgress: {file.progress_string()}🔄 "
-                msg += f"\nTotal Size: {file.total_length_string()}🗂️ "
+                msg += f"\nProgress: {file.progress_string()} 🔄 "
+                msg += f"\nTotal Size: {file.total_length_string()} 🗂️ "
 
                 if is_file is None :
-                   msg += f"\n<b>Connections:</b> {file.connections}📶 "
+                   msg += f"\n<b>Connections:</b> {file.connections} 📶 "
                 else :
                    msg += f"\n<b>Info:</b>[ P : {file.connections} || S : {file.num_seeders} ]"
 
