a4dff2d908f485843c3b3cdca12cc35cbada0b89
cdfxscrq <lmstream@telegmail.com>
2020-08-01 18:12:21 +0530

    update

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -51,6 +51,7 @@ async def aria_start():
     aria2_daemon_start_cmd.append("--seed-ratio=0")
     aria2_daemon_start_cmd.append("--seed-time=0")
     aria2_daemon_start_cmd.append("--split=10")
+    aria2_daemon_start_cmd.append("--bt-max-peers=0")
     aria2_daemon_start_cmd.append(f"--bt-stop-timeout={MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START}")
     #
     LOGGER.info(aria2_daemon_start_cmd)
